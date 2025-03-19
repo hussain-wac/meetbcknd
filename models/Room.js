f4f0bcf973e734588a185b191962dbb0befe73fc
hussain-wac <hussain.n@webandcrafts.in>
2025-03-19 10:20:07 +0530

    avil

--- models/Room.js
+++ models/Room.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
   roomId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  capacity: { type: Number, required: true },
+  features: [{ type: String }], // e.g., ["Video", "Whiteboard"]
   meetings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meeting' }],
-  totalAvailableMinutes: { type: Number, default: 1440 }, // Store available minutes in a day
-  availabilityPercentage: { type: Number, default: 100 } // Store availability percentage
+  totalAvailableMinutes: { type: Number, default: 1440 },
+  availabilityPercentage: { type: Number, default: 100 }
 });
 
 module.exports = mongoose.model('Room', roomSchema);
