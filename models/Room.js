81cdb645bc0f4638c01196b8f66e8f70ce7d141a
hussain-wac <hussain.n@webandcrafts.in>
2025-03-19 10:04:09 +0530

    room code

--- models/Room.js
+++ models/Room.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
-  roomId: { type: String, required: true },
-  meetings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meeting' }] // Store meeting references here
+  roomId: { type: String, required: true, unique: true },
+  meetings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meeting' }],
+  totalAvailableMinutes: { type: Number, default: 1440 }, // Store available minutes in a day
+  availabilityPercentage: { type: Number, default: 100 } // Store availability percentage
 });
 
 module.exports = mongoose.model('Room', roomSchema);
